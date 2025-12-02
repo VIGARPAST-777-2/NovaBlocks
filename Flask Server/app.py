f0d985a7bd74523ce13f576fd680f4940f53be68
VIGARPAST_777 <vicgarpas3@alu.edu.gva.es>
2025-12-02 20:25:56 +0100

    Update app.py

--- Flask Server/app.py
+++ Flask Server/app.py
@@ -6,26 +6,21 @@ WEB_DIR = os.path.join(BASE_DIR, "Visual Programming Interface")
 
 app = Flask(__name__)
 
-# MENÚ
 @app.route("/")
 def menu():
     return send_from_directory(os.path.join(WEB_DIR, "menu"), "index.html")
 
-# EDITOR
 @app.route("/editor")
 def editor():
     return send_from_directory(os.path.join(WEB_DIR, "editor"), "index.html")
 
-# ARCHIVOS ESTÁTICOS DEL MENÚ
-@app.route("/menu/<path:path>")
-def menu_files(path):
-    return send_from_directory(os.path.join(WEB_DIR, "menu"), path)
+@app.route("/assets/<path:path>")
+def assets(path):
+    return send_from_directory(os.path.join(WEB_DIR, "assets"), path)
 
-# ARCHIVOS ESTÁTICOS DEL EDITOR
-@app.route("/editor/<path:path>")
-def editor_files(path):
-    return send_from_directory(os.path.join(WEB_DIR, "editor"), path)
-
+@app.route("/editor/assets/<path:path>")
+def editor_assets(path):
+    return send_from_directory(os.path.join(WEB_DIR, "editor/assets"), path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
