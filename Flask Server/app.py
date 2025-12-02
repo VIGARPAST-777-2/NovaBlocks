f625954d9bbcee6ce1204dd1da22ff6695b2318a
VIGARPAST_777 <vicgarpas3@alu.edu.gva.es>
2025-12-02 20:52:03 +0100

    Update app.py

--- Flask Server/app.py
+++ Flask Server/app.py
@@ -2,25 +2,17 @@ from flask import Flask, send_from_directory
 import os
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-WEB_DIR = os.path.join(BASE_DIR, "Visual Programming Interface")
+VPI_DIR = os.path.join(BASE_DIR, "Visual Programming Interface")
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 
 @app.route("/")
-def menu():
-    return send_from_directory(os.path.join(WEB_DIR, "menu"), "index.html")
+def root():
+    return send_from_directory(os.path.join(VPI_DIR, "menu"), "index.html")
 
-@app.route("/editor")
-def editor():
-    return send_from_directory(os.path.join(WEB_DIR, "editor"), "index.html")
-
-@app.route("/assets/<path:path>")
-def assets(path):
-    return send_from_directory(os.path.join(WEB_DIR, "assets"), path)
-
-@app.route("/editor/assets/<path:path>")
-def editor_assets(path):
-    return send_from_directory(os.path.join(WEB_DIR, "editor/assets"), path)
+@app.route("/Visual Programming Interface/<path:path>")
+def serve_vpi(path):
+    return send_from_directory(VPI_DIR, path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
